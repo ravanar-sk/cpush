1d9bea9899f24c4c360d4784e3f93be094ce8506
Saravana Kumar K R <development.ravanar@gmail.com>
2024-04-24 00:26:21 +0300

    APNS using p8 file implementation

--- src/apns/apns.js
+++ src/apns/apns.js
@@ -43,38 +43,43 @@ const generateAuthToken = async (privateKeyString, keyID, teamID) => {
  * @returns 
  */
 function convertPFX(pfx, passphrase) {
+    try {
 
-    let p12buffer = ''
+        let p12buffer = ''
 
-    if (Buffer.isBuffer(pfx)) {
-        p12buffer = pfx.toString('base64');
-    } else {
-        p12buffer = pfx;
+        if (Buffer.isBuffer(pfx)) {
+            p12buffer = pfx.toString('base64');
+        } else {
+            p12buffer = pfx;
+        }
+
+        const asn = forge.asn1.fromDer(forge.util.decode64(p12buffer));
+        const p12 = forge.pkcs12.pkcs12FromAsn1(asn, true, passphrase);
+
+        const keyData = p12.getBags({ bagType: forge.pki.oids.pkcs8ShroudedKeyBag })[forge.pki.oids.pkcs8ShroudedKeyBag]
+            .concat(p12.getBags({ bagType: forge.pki.oids.keyBag })[forge.pki.oids.keyBag]);
+        const certBags = p12.getBags({ bagType: forge.pki.oids.certBag })[forge.pki.oids.certBag];
+
+        console.log(keyData);
+
+        const privateKey = keyData[0].key
+        // convert a Forge private key to an ASN.1 RSAPrivateKey
+        const rsaPrivateKey = forge.pki.privateKeyToAsn1(privateKey);
+
+        // wrap an RSAPrivateKey ASN.1 object in a PKCS#8 ASN.1 PrivateKeyInfo
+        const privateKeyInfo = forge.pki.wrapRsaPrivateKey(rsaPrivateKey);
+
+        // convert a PKCS#8 ASN.1 PrivateKeyInfo to PEM
+        const pemKey = forge.pki.privateKeyInfoToPem(privateKeyInfo);
+
+        return {
+            certificate: forge.pki.certificateToPem(certBags[0].cert),
+            key: pemKey
+        };
     }
-
-    const asn = forge.asn1.fromDer(forge.util.decode64(p12buffer));
-    const p12 = forge.pkcs12.pkcs12FromAsn1(asn, true, passphrase);
-
-    const keyData = p12.getBags({ bagType: forge.pki.oids.pkcs8ShroudedKeyBag })[forge.pki.oids.pkcs8ShroudedKeyBag]
-        .concat(p12.getBags({ bagType: forge.pki.oids.keyBag })[forge.pki.oids.keyBag]);
-    const certBags = p12.getBags({ bagType: forge.pki.oids.certBag })[forge.pki.oids.certBag];
-
-    console.log(keyData);
-
-    const privateKey = keyData[0].key
-    // convert a Forge private key to an ASN.1 RSAPrivateKey
-    const rsaPrivateKey = forge.pki.privateKeyToAsn1(privateKey);
-
-    // wrap an RSAPrivateKey ASN.1 object in a PKCS#8 ASN.1 PrivateKeyInfo
-    const privateKeyInfo = forge.pki.wrapRsaPrivateKey(rsaPrivateKey);
-
-    // convert a PKCS#8 ASN.1 PrivateKeyInfo to PEM
-    const pemKey = forge.pki.privateKeyInfoToPem(privateKeyInfo);
-
-    return {
-        certificate: forge.pki.certificateToPem(certBags[0].cert),
-        key: pemKey
-    };
+    catch (e) {
+        console.log(e);
+    }
 }
 
 const apnsAPI = async (
@@ -91,8 +96,20 @@ const apnsAPI = async (
         password = ''
     }) => {
 
+    console.log(`
+isDevelopment : ${isDevelopment}
+t_header : ${JSON.stringify(t_header)}
+body : ${JSON.stringify(body)}
+deviceToken : ${deviceToken}
+isP12 : ${isP12}
+privateKey : ${privateKey}
+keyID : ${keyID}
+teamID : ${teamID}
+buffer : ${buffer}
+password : ${password}
+`)
     return new Promise(async (resolve, reject) => {
-        
+
         const baseURL = isDevelopment ? developmentURL : productionURL
         const endPath = `/3/device/${deviceToken}`
 
@@ -105,6 +122,12 @@ const apnsAPI = async (
 
         let client = null
 
+        console.log(`
+        baseURL : ${baseURL}
+        endPath : ${endPath}
+        headers : ${JSON.stringify(headers)}
+        `)
+
         if (isP12) {
             const { key, certificate } = convertPFX(buffer, password);
             client = http2.connect(baseURL, {
@@ -114,8 +137,9 @@ const apnsAPI = async (
         } else {
             client = http2.connect(baseURL)
             const authToken = await generateAuthToken(privateKey, keyID, teamID);
-            const bearerToken = `bearer ${authToken}`
-            headers.authorization = bearerToken
+            console.log(`authToken : ${authToken}`);
+            const bearerToken = `Bearer ${authToken}`
+            headers.Authorization = bearerToken
         }
 
         client.on('error', (err) => {
@@ -125,22 +149,29 @@ const apnsAPI = async (
 
         const request = client.request(headers);
 
-        request.on('response', (headers, flags) => {
-            for (const name in headers) {
-                console.log(`${name}: ${headers[name]}`);
-            }
-        });
-
         request.setEncoding('utf8');
 
+        request.on('response', (headers, flags) => {
+
+            console.log(`RESPONSE : ${JSON.stringify(headers)}`)
+            // for (const name in headers) {
+            //     console.log(`${name}: ${headers[name]}`);
+            // }
+        });
+
         let data = ''
-        request.on('data', (chunk) => { data += chunk; });
-        request.write(JSON.stringify(body))
+        request.on('data', (chunk) => { 
+            console.log('DATA : ')
+            data += chunk; 
+        });
+
         request.on('end', () => {
-            console.log(`\n${data}`);
+            console.log(`\n END : ${data}`);
             resolve(data);
             client.close();
         });
+
+        request.write(JSON.stringify(body))
         request.end();
     });
 }
